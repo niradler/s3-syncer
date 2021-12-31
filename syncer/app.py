1a970f34d2fa19314e030fe71dc2a7a2b80693b7
Nir Adler <niradler@users.noreply.github.com>
2021-12-31 15:17:51 +0200

    update readme

--- syncer/app.py
+++ syncer/app.py
@@ -5,12 +5,12 @@ from botocore.client import Config
 
 s3 = boto3.resource(
     's3',
-    # aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID', 'Q4X2N1H22UGQDSK*'),
-    # aws_secret_access_key=os.getenv(
-    #     'AWS_ACCESS_KEY_ID', 'KTYTBE125EP3NW3005ZXGYVAYZMEW24*'),
-    # region_name=os.getenv('AWS_DEFAULT_REGION', 'us-east-1'),
-    # config=Config(signature_version='s3v4'),
-    # endpoint_url='https://s3.us-east-1.lyvecloud.seagate.com'
+    aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID', 'TEST_KEY_ID'),
+    aws_secret_access_key=os.getenv(
+        'AWS_ACCESS_KEY_SECRET', 'TEST_KEY_SECRET'),
+    region_name=os.getenv('AWS_DEFAULT_REGION', 'us-east-1'),
+    config=Config(signature_version='s3v4'),
+    endpoint_url='https://s3.us-east-1.lyvecloud.seagate.com'
 )
 
 sourceS3 = boto3.resource('s3')
@@ -37,7 +37,6 @@ def handle_create_object(event):
         bucket = sourceS3.Bucket(source_bucket)
         file_path = '/tmp/tempfile'
         app.log.info(file_path)
-        print(event.key)
         os.chdir('/tmp')
         bucket.download_file(event.key, file_path)
         bucket = s3.Bucket(target_bucket)
@@ -48,7 +47,7 @@ def handle_create_object(event):
 @app.on_s3_event(bucket=source_bucket, prefix=source_prefix, events=['s3:ObjectRemoved:*'])
 def handle_remove_object(event):
     event_dic = event.to_dict()
-    print(event_dic)
+    app.log.info(event_dic)
     action_name = event_dic['Records'][0]['eventName']
     app.log.info("Event received for bucket: %s, key %s, eventName %s",
                  event.bucket, event.key, action_name)
